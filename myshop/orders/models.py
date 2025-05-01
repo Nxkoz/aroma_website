c12aed6bda9b094b5bbdfc84aadf18fb953e35ee
Nxkoz <kozachekns@gmail.com>
2025-05-01 22:44:27 +0300

    added model orders

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    address = models.CharField(max_length=250)
+    phone = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id}"
