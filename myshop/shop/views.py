9ccae6d3f734804fd3b2402235a686126a7c85ab
Nxkoz <kozachekns@gmail.com>
2025-05-01 12:00:39 +0300

    added imports and performances for adding in "cart"

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Product
+from cart.cart import Cart
+from django.contrib import messages
+
+
 
 def product_list(request):
     products = Product.objects.all()
@@ -16,3 +20,9 @@ def index(request):
 def home(request):
     return render(request, 'shop/home.html')
 
+def add_to_cart(request, id):
+    cart = Cart(request)
+    product = get_object_or_404(Product, id=id)
+    cart.add(product=product, quantity=1)
+    messages.success(request, f'Товар "{product.name}" додано в кошик!')
+    return redirect('shop:product_detail', id=id)
