7aeb6c7451dc4b7dbb052d8f18439b354b6ec43a
Nxkoz <kozachekns@gmail.com>
2025-05-01 22:49:54 +0300

    Added checkout view

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import OrderCreateForm
+from cart.cart import Cart
 
-# Create your views here.
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            # тут можно добавить логику сохранения товаров, если используем OrderItem
+            cart.clear()
+            return render(request, 'orders/order_created.html', {'order': order})
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order_create.html', {'cart': cart, 'form': form})
