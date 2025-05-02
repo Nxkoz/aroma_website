19db378bc88d2f24233d937e224f9ee22f0be5aa
Nxkoz <kozachekns@gmail.com>
2025-05-02 17:15:39 +0300

    updated shop/urls.py

--- myshop/shop/urls.py
+++ myshop/shop/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView, add_to_cart
+from .views import ProductListView, ProductDetailView, add_to_cart, home
 
 app_name = 'shop'
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='product_list'),  # Каталог
-    path('product/<int:id>/', ProductDetailView.as_view(), name='product_detail'),  # Детали товара
+    path('', home, name='home'),
+    path('catalog/', ProductListView.as_view(), name='product_list'),
+    path('product/<int:id>/', ProductDetailView.as_view(), name='product_detail'),
     path('cart/add/<int:id>/', add_to_cart, name='add_to_cart'),
 ]
